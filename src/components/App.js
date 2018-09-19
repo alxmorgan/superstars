7d08730264861ce86276dbfea06361e3cdb75acf
Alan Morgan <a.morgan@pilz.ie>
2018-09-19 18:03:10 +0100

    Sizing

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
           </div>
           <div className="cards">
               <Player name="Cristiano Ronaldo" club="Juventus" move="Stepover" image={Ronaldo} link="https://www.youtube.com/watch?v=9yFcicZYpg4&list=PLTNn_IfgATHu2eQZvYLA1sos7kFZaH8m1"/>
-              <Player name="Andres Iniesta" club="Barcelona" move="La Croqueta" image={Iniesta} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
+              {/*<Player name="Andres Iniesta" club="Barcelona" move="La Croqueta" image={Iniesta} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>*/}
               <Player  image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
               <Player  image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
               <Player image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
