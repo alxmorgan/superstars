ff0632828970a2254274c8fb90b1607708e939e0
Alan Morgan <a.morgan@pilz.ie>
2018-10-10 18:32:37 +0100

    Messi

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import '../index.css';
 import Iniesta from '../images/iniesta.jpeg';
 import Ronaldo from '../images/ronaldo.jpeg';
 import Unknown from '../images/Unknown.jpg';
+import Messi from '../images/messi.jpg';
 
 class App extends Component {
   render() {
@@ -17,7 +18,7 @@ class App extends Component {
           <div className="cards">
               <Player name="Cristiano Ronaldo" club="Juventus" move="Stepover" image={Ronaldo} link="https://v637g.app.goo.gl/k9TK5BhuYAGq58JYA"/>
               {/*<Player name="Andres Iniesta" club="Barcelona" move="La Croqueta" image={Iniesta} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>*/}
-              <Player  image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
+              <Player name="Lionel Messi" club="Barcelona" move="Body Feint" image={Messi} link="https://v637g.app.goo.gl/m8rQ6tTSzLVcpqEW7"/>
               <Player  image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
               <Player image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
               <Player image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
