605ce3dbd58991ef96167f92b72f5f9538f87365
Alan Morgan <a.morgan@pilz.ie>
2018-11-14 19:56:29 +0000

    Add mahrez

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Ronaldo from '../images/ronaldo.jpeg';
 import Unknown from '../images/Unknown.jpg';
 import Messi from '../images/messi.jpg';
 import Matthews from '../images/matthews.jpg';
+import Mahrez from '../images/mahrez.jpg';
 
 class App extends Component {
   render() {
@@ -21,7 +22,7 @@ class App extends Component {
               {/*<Player name="Andres Iniesta" club="Barcelona" move="La Croqueta" image={Iniesta} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>*/}
               <Player name="Lionel Messi" club="Barcelona" move="Body Feint" image={Messi} link="https://v637g.app.goo.gl/m8rQ6tTSzLVcpqEW7"/>
               <Player name="Stanley Matthews" club="Blackpool" move="The Matthews" image={Matthews} link="https://v637g.app.goo.gl/nhWUGAq43BF7Crxs9"/>
-              <Player  image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
+              <Player name="Riyad Mahrez" club="Man City" move="Fake Shot" image={Mahrez} link="https://v637g.app.goo.gl/3wc4BNX1xiUS3krF6"/>
               <Player image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
               <Player image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
               <Player  image={Unknown} link="https://www.youtube.com/embed/Wq-hhEUO4eM?start=20&end=25&version=3"/>
